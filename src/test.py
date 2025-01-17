a9591ce2da630957e94c3bdf114b49a6684e8389
Daniil Temnyj <temnyy1@gmail.com>
2025-01-17 18:46:23 +0300

    Tuned params to b_vamp counter recognition, using frame crops

--- src/test.py
+++ src/test.py
@@ -6,7 +6,7 @@ import os
 import cv2
 import numpy as np
 
-def detect_symbols(im, symbols_height_threshold=10):
+def detect_symbols(im, symbols_height_threshold=10, symbols_width_threshold=10):
     script_folder = os.path.dirname(os.path.abspath(__file__))
 
     samples = np.loadtxt(os.path.join(script_folder, '..', 'data', 'generalsamples.data'), np.float32)
@@ -17,28 +17,53 @@ def detect_symbols(im, symbols_height_threshold=10):
     model.train(samples, cv2.ml.ROW_SAMPLE, responses)
 
     out = np.zeros(im.shape, np.uint8)
+    
     gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 15, 2)
+    gray = cv2.bitwise_not(gray)
+    _, thresh = cv2.threshold(gray, 100, 255, 0)
+    
+    # gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
+    # # gray = cv2.bitwise_not(gray)
+    # # blur = gray
+    # blur = cv2.GaussianBlur(gray, (5, 5), 0)
+    # thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 3, 2)
+    # # _, thresh = cv2.threshold(blur, 150, 255, 0)
+
+
+    # cv2.imshow('thresh', thresh)
 
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     symbol_sequence_result = ''
     symbols_positions = {}
 
+    widths = []
+
     for cnt in contours:
         [x, y, w, h] = cv2.boundingRect(cnt)
-        if h > symbols_height_threshold:
+        if h > symbols_height_threshold and w < symbols_width_threshold:
             im = cv2.rectangle(im, (x, y), (x + w, y + h), (0, 255, 0), 2)
             roi = thresh[y:y + h, x:x + w]
             roismall = cv2.resize(roi, (10, 10))
             roismall = roismall.reshape((1, 100))
             roismall = np.float32(roismall)
             retval, results, neigh_resp, dists = model.findNearest(roismall, k=1)
+            if dists > 1300000:
+                # print(retval, dists)
+                continue
             string = str(int((results[0][0])))
-            symbols_positions[(x, y)] = string
-            cv2.putText(out, string, (x, y + h), 0, 0.5, (0, 255, 0))
+            if string != '-1':
+                widths.append(w)
+                symbols_positions[(x, y)] = string
+                cv2.putText(out, string, (x, y + h), 0, 0.5, (0, 255, 0))
 
+    avg_width = 0# sum(widths)/len(widths)
+    skip = True
+    prew_pos = [-1, -1]
     for pos in sorted(symbols_positions.keys()):
-        symbol_sequence_result += symbols_positions[pos]
+        if skip or pos[0] - prew_pos[0] > avg_width/2:
+            symbol_sequence_result += symbols_positions[pos]
+            skip = False
+        prew_pos = pos
 
     return out, symbol_sequence_result
 
@@ -46,7 +71,7 @@ if __name__ == "__main__":
     script_folder = os.path.dirname(os.path.abspath(__file__))
     im = cv2.imread(os.path.join(script_folder, '..','data', 'test2.png'))
 
-    out, symbol_sequence_result = detect_symbols(im)
+    out, symbol_sequence_result = detect_symbols(im, 15)
 
     print(symbol_sequence_result)
 
--- src/train.py
+++ src/train.py
@@ -7,39 +7,65 @@ import sys
 import numpy as np
 import cv2
 
-def train_symbols(im, symbol_height_threshold=17):
+def train_symbols(im, symbol_height_threshold=15):
     script_folder = os.path.dirname(os.path.abspath(__file__))
-    im3 = im.copy()
-
-    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    blur = cv2.GaussianBlur(gray, (5, 5), 0)
-    thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 3, 2)
-
-    cv2.imshow('test_threshold', thresh)
-
-    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
     samples = np.empty((0, 100), np.float32)
     responses = []
     keys = [i for i in range(48, 58)]
 
-    for cnt in contours:
-        [x, y, w, h] = cv2.boundingRect(cnt)
+    frames_folder_path = os.path.join(script_folder, '..', 'data', 'frames')
 
-        if h > symbol_height_threshold:
-            im = cv2.rectangle(im, (x, y), (x + w, y + h), (255, 0, 0), 2)
-            roi = thresh[y:y + h, x:x + w]
-            roismall = cv2.resize(roi, (10, 10))
-            cv2.imshow('press corresponding digit', im)
-            key = cv2.waitKey(0)
+    frames = next(os.walk(frames_folder_path), (None, None, []))[2]  # [] if no file
+    
+    
+    for frame_file in frames:
+        frame = cv2.imread(os.path.join(frames_folder_path, frame_file))
+        
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray = cv2.bitwise_not(gray)
+        _, thresh = cv2.threshold(gray, 100, 255, 0)
 
-            if key == 27:  # (escape to quit)
-                sys.exit()
-            elif key in keys:
-                responses.append(int(chr(key)))
-                sample = roismall.reshape((1, 100))
-                samples = np.append(samples, sample, 0)
-            cv2.rectangle(im, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # # gray = cv2.bitwise_not(gray)
+        # # blur = gray
+        # blur = cv2.GaussianBlur(gray, (5, 5), 0)
+        # thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY_INV, 3, 2)
+        # # _, thresh = cv2.threshold(blur, 150, 255, 0)
+
+        cv2.imshow('test_threshold', thresh)
+        # cv2.waitKey(0)
+
+        contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        key = 0
+
+        for cnt in contours:
+            [x, y, w, h] = cv2.boundingRect(cnt)
+
+            if h > symbol_height_threshold:
+                frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                roi = thresh[y:y + h, x:x + w]
+                roismall = cv2.resize(roi, (10, 10))
+                cv2.imshow('press corresponding digit', frame)
+                key = cv2.waitKey(0)
+
+                if key == 27:  # (escape to quit)
+                    break
+                    # sys.exit()
+                elif key in keys:
+                    responses.append(int(chr(key)))
+                    sample = roismall.reshape((1, 100))
+                    samples = np.append(samples, sample, 0)
+                else:
+                    responses.append(-1)
+                    sample = roismall.reshape((1, 100))
+                    samples = np.append(samples, sample, 0)
+
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                
+        if key == 27:  # (escape to quit)
+            break
+
     responses = np.array(responses, np.float32)
     responses = responses.reshape((responses.size, 1))
     print('training complete')
